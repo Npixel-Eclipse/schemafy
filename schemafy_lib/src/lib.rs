6057a09d87a28fd45a3ca4acf192128f09cc15df
notinmybackyaard <theloder@naver.com>
2022-05-30 14:18:00 +0900

    Change function compile_schemas

--- schemafy_lib/src/lib.rs
+++ schemafy_lib/src/lib.rs
@@ -59,7 +59,7 @@ pub mod generator;
 /// This module is itself generated from a JSON schema.
 mod schema;
 
-use std::{io, env, path::Path,  borrow::Cow, convert::TryFrom};
+use std::{env, path::Path,  borrow::Cow, convert::TryFrom};
 
 use inflector::Inflector;
 
@@ -788,34 +788,41 @@ impl<'r> Expander<'r> {
     }
 }
 
-pub fn compile_schemas(input_path: &str) -> io::Result<()> {
+pub fn compile_schemas(input_path: &str) {
     let input_path = Path::new(input_path);
+    let input_file_name = input_path.file_name().unwrap();
+    let input_file_name = input_file_name.to_str().unwrap();
 
-    // read schema files
-    let input_files: Vec<_> = input_path.read_dir()?
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.path().to_str().unwrap().ends_with(".schema.yaml"))
+    let first_dot_pos = input_file_name.find('.').unwrap();
+    let input_file_extenstion = &input_file_name[first_dot_pos..];
+
+    let target_path = input_path.parent().unwrap();
+
+    let current_path = env::current_dir().unwrap();
+    let filtered : Vec<_> = current_path.ancestors()
+        .map(|path| path.join(target_path))
+        .filter(|path| path.exists())
+        .flat_map(|path| path.read_dir().unwrap())
+        .filter_map(|path| path.ok())
+        .filter(|path| path.file_name().to_str().unwrap().contains(input_file_extenstion))
         .collect();
 
     let output_path = env::var("OUT_DIR").unwrap();
     let output_path = Path::new(&output_path);
 
     // generate rust code files
-    for entry in input_files {
-        if let Some(input_file_name) = entry.path().file_name() {
-            let prefix_name: String = input_file_name.to_string_lossy().split('.').take(1).collect();
+    for entry in filtered {
+            let input_file_name = entry.file_name().into_string().unwrap();
+            let prefix_name: String = input_file_name.split('.').take(1).collect();
             let output_file_name = output_path.join(format!("{}.rs", &prefix_name));
 
             Generator::builder()
                 .with_root_name_str(&prefix_name)
-                .with_input_file(entry.path().as_path())
+                .with_input_file(&entry.path())
                 .build()
-                .generate_to_file(&output_file_name.as_path())
+                .generate_to_file(&output_file_name)
                 .unwrap();
-        }
     }
-
-    Ok(())
 }
 
 #[cfg(test)]
